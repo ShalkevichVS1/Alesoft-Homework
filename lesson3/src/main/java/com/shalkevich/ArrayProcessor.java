6198a2cc0a0ba1542df08a8d74d90c83c5427163
Vlad Shalkevich <vladshsmile@gmail.com>
2025-05-07 13:59:28 +0300

    The third homework is ready

---
+++ lesson3/src/main/java/com/shalkevich/ArrayProcessor.java
@@ -0,0 +1,22 @@
+package com.shalkevich;
+
+public class ArrayProcessor {
+
+    public static int processArray(String[][] array) {
+        if (array.length != 4 || array[0].length != 4) {
+            throw new MyArraySizeException("Массив должен быть размером 4x4");
+        }
+
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                try {
+                    sum += Integer.parseInt(array[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException("Ошибка в ячейке [" + i + "][" + j + "]: неверные данные \"" + array[i][j] + "\"");
+                }
+            }
+        }
+        return sum;
+    }
+}
